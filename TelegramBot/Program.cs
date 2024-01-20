d6785e10c1f2326ef374b60b69ac273df56dc1ef
German Gritsenko <kyperu07@gmail.com>
2024-01-20 23:27:08 +0200

    create OpenAI API KEY

--- TelegramBot/Program.cs
+++ TelegramBot/Program.cs
@@ -1,13 +1,13 @@
-﻿using System;
+﻿using System.Net.Http.Json;
+using System.Text.Json.Serialization;
+using Telegram.Bot.Types;
 
-namespace TelegramBot
-{
-    class Program
-    {
-        static void Main()
-        {
-            
-        }
-    }
-}
+// токен из личного кабинета
+string apiKey = "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";
+// адрес api для взаимодействия с чат-ботом
+string endpoint = "https://api.openai.com/v1/chat/completions";
+// набор соообщений диалога с чат-ботом
+List<Message> messages = new List<Message>();
 
+var httpClient =  new HttpClient();
+httpClient.DefaultRequestHeaders.Add("Authorization", $"Bearer{apiKey}");
